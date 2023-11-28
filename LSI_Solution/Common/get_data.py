e94f343311fcb86eca2f9c7e220888316cf919c4
DSROH <95155653+DSROH@users.noreply.github.com>
2023-11-28 19:56:33 +0900

    Update get_data.py

--- LSI_Solution/Common/get_data.py
+++ LSI_Solution/Common/get_data.py
@@ -17,7 +17,7 @@ import LSI_Solution.DASEUL.ET_Sub6 as Lsub6et
 import LSI_Solution.DASEUL.FBRX as Lfbrx
 import LSI_Solution.DASEUL.APT as Lapt
 import LSI_Solution.Common.Function as func
-import LSI_Solution.DASEUL.Drawing as Ldraw
+# import LSI_Solution.DASEUL.Drawing as Ldraw
 
 
 def rfic_gain_average(df_rfic_gain, Save_data, text_area):
@@ -41,7 +41,7 @@ def rfic_gain_average(df_rfic_gain, Save_data, text_area):
     return df_rfic_gain_Mean["Average"]
 
 
-def get_data(list_file, Select_op, Save_data_var, debug_var, raw_data_var, get_data_var, text_area):
+def get_data(list_file, Select_op, Selected_Option, Save_data_var, debug_var, raw_data_var, get_data_var, text_area):
     if list_file.size() == 0:
         msgbox.showwarning("경고", "Cal log 파일(*.csv)을 추가하세요")
         return
@@ -172,6 +172,217 @@ def get_data(list_file, Select_op, Save_data_var, debug_var, raw_data_var, get_d
 
             Total_count += i
 
+        Result_Meas.dropna(how="all", axis=1, inplace=True)
+        Result_Code.dropna(how="all", axis=1, inplace=True)
+
+        text_area.insert(tk.END, "=" * 85)
+        text_area.insert(tk.END, "\n")
+        text_area.see(tk.END)
+
+        if raw_data_var.get():
+            filename = "Result_Meas.xlsx"
+            with pd.ExcelWriter(filename) as writer:
+                Result_Meas.to_excel(writer, sheet_name="Meas_Full_Data")
+            wb = load_workbook(filename)
+            ws = wb.sheetnames
+            for sheet in ws:
+                col_max = wb[sheet].max_column
+                row_max = wb[sheet].max_row
+                text_area.insert(tk.END, f"{sheet:<30}| Col = {col_max:<5}, Row = {row_max:<5} | ")
+                text_area.see(tk.END)
+            text_area.insert(tk.END, f"Done\n")
+            text_area.see(tk.END)
+
+            filename = "Result_Code.xlsx"
+            with pd.ExcelWriter(filename) as writer:
+                Result_Code.to_excel(writer, sheet_name="Code_Full_Data")
+            wb = load_workbook(filename)
+            ws = wb.sheetnames
+            for sheet in ws:
+                col_max = wb[sheet].max_column
+                row_max = wb[sheet].max_row
+                text_area.insert(tk.END, f"{sheet:<30}| Col = {col_max:<5}, Row = {row_max:<5} | ")
+                text_area.see(tk.END)
+            text_area.insert(tk.END, f"Done\n")
+            text_area.see(tk.END)
+
+        if get_data_var.get():
+            Save_data = True
+        else:
+            Save_data = Save_data_var.get()
+
+        DC_Cal_Mean, IIP2_Cal_Mean, DC_Cal_3G, DC_Cal_2G, DC_Cal_NR = Ldciip2.daseul_dc_iip2(Result_Code, Save_data, text_area)
+        CableCheck = Lcable.daseul_cable_average(Result_Meas, Save_data, text_area)
+        RFIC_gain = Lget.rfic_gain_average(df_RFIC_gain, Save_data, text_area)
+        TxP_Channel_comp_ave = L3gtx.TxP_3g_channel_comp_pa_mid(Result_Meas, Save_data, text_area)
+        (
+            FBRX_Gain_Meas_3G,
+            FBRX_Gain_Meas_sub6,
+            FBRX_Gain_Code_3G,
+            FBRX_Gain_Code_sub6,
+            FBRX_Freq_Meas_3G,
+            FBRX_Freq_Meas_3G_Max,
+            FBRX_Freq_Meas_3G_Min,
+            FBRX_Freq_Meas_sub6,
+            FBRX_Freq_Meas_sub6_Max,
+            FBRX_Freq_Meas_sub6_Min,
+            FBRX_Freq_Code_sub6,
+            FBRX_Freq_Code_sub6_Max,
+            FBRX_Freq_Code_sub6_Min,
+        ) = Lfbrx.fbrx_average(Result_Meas, Result_Code, Save_data, text_area)
+        PRX_Gain_2G, Ripple_2G, RXGain_3G, RXComp_3G, RXGain_sub6, RXRSRP_sub6, RXComp_sub6 = Lrx_ave.daseul_rx_average(
+            Result_Meas, Save_data, text_area
+        )
+        GMSK_Mean, GMSK_TXL_Mean, GMSK_Code_Mean = L2gtx.gsm_tx_gmsk_average(Result_Meas, Result_Code, Save_data, text_area)
+        EPSK_Mean, EPSK_TXL_Mean, EPSK_Code_Mean = L2gtx.gsm_tx_edge_average(Result_Meas, Result_Code, Save_data, text_area)
+
+        (
+            ETSAPT_3G_Psat_Ave,
+            ETSAPT_3G_Psat_Max,
+            ETSAPT_3G_Psat_Min,
+            ETSAPT_3G_Power_Ave,
+            ETSAPT_3G_Power_Max,
+            ETSAPT_3G_Power_Min,
+        ) = L3get.Et_3g_average(Result_Meas, Save_data, text_area)
+
+        thermistor = Lsub6tx.therm_average(Result_Code, Save_data, text_area)
+
+        APT_3G_Ave, APT_Sub6_Ave, APT_Sub6_Max, APT_Sub6_Min = Lapt.apt_average(Result_Meas, Save_data, text_area)
+
+        (
+            ETSAPT_sub6_Psat_Ave,
+            ETSAPT_sub6_Psat_Max,
+            ETSAPT_sub6_Psat_Min,
+            ETSAPT_sub6_Freq_Ave,
+            ETSAPT_sub6_Freq_Max,
+            ETSAPT_sub6_Freq_Min,
+            ETSAPT_sub6_Pgain_Ave,
+            ETSAPT_sub6_Pgain_Max,
+            ETSAPT_sub6_Pgain_Min,
+            ETSAPT_sub6_Power_Ave,
+            ETSAPT_sub6_Power_Max,
+            ETSAPT_sub6_Power_Min,
+        ) = Lsub6et.sub6_et_average(Result_Meas, Save_data, text_area)
+        BW_Cal = Lsub6tx.sub6_bw_cal_average(Result_Meas, Save_data, text_area)
+
+        # ! Plotly
+        # Ldraw.Drawing_dc_cal(DC_Cal_Mean)
+
+        if get_data_var.get() & (Selected_Option == 2):
+            return (
+                CableCheck,
+                RFIC_gain,
+                TxP_Channel_comp_ave,
+                FBRX_Gain_Meas_3G,
+                FBRX_Gain_Meas_sub6,
+                FBRX_Gain_Code_3G,
+                FBRX_Gain_Code_sub6,
+                FBRX_Freq_Meas_3G,
+                FBRX_Freq_Meas_3G_Max,
+                FBRX_Freq_Meas_3G_Min,
+                FBRX_Freq_Meas_sub6,
+                FBRX_Freq_Meas_sub6_Max,
+                FBRX_Freq_Meas_sub6_Min,
+                FBRX_Freq_Code_sub6,
+                FBRX_Freq_Code_sub6_Max,
+                FBRX_Freq_Code_sub6_Min,
+                PRX_Gain_2G,
+                Ripple_2G,
+                RXGain_3G,
+                RXComp_3G,
+                RXGain_sub6,
+                RXRSRP_sub6,
+                RXComp_sub6,
+                GMSK_Mean,
+                GMSK_TXL_Mean,
+                GMSK_Code_Mean,
+                EPSK_Mean,
+                EPSK_TXL_Mean,
+                EPSK_Code_Mean,
+                ETSAPT_3G_Psat_Ave,
+                ETSAPT_3G_Psat_Max,
+                ETSAPT_3G_Psat_Min,
+                ETSAPT_3G_Power_Ave,
+                ETSAPT_3G_Power_Max,
+                ETSAPT_3G_Power_Min,
+                thermistor,
+                APT_3G_Ave,
+                APT_Sub6_Ave,
+                APT_Sub6_Max,
+                APT_Sub6_Min,
+                ETSAPT_sub6_Psat_Ave,
+                ETSAPT_sub6_Psat_Max,
+                ETSAPT_sub6_Psat_Min,
+                ETSAPT_sub6_Freq_Ave,
+                ETSAPT_sub6_Freq_Max,
+                ETSAPT_sub6_Freq_Min,
+                ETSAPT_sub6_Pgain_Ave,
+                ETSAPT_sub6_Pgain_Max,
+                ETSAPT_sub6_Pgain_Min,
+                ETSAPT_sub6_Power_Ave,
+                ETSAPT_sub6_Power_Max,
+                ETSAPT_sub6_Power_Min,
+                BW_Cal,
+            )
+        elif get_data_var.get():
+            msgbox.showwarning("Message", "작업 완료")
+        else:
+            return (
+                CableCheck,
+                RFIC_gain,
+                TxP_Channel_comp_ave,
+                FBRX_Gain_Meas_3G,
+                FBRX_Gain_Meas_sub6,
+                FBRX_Gain_Code_3G,
+                FBRX_Gain_Code_sub6,
+                FBRX_Freq_Meas_3G,
+                FBRX_Freq_Meas_3G_Max,
+                FBRX_Freq_Meas_3G_Min,
+                FBRX_Freq_Meas_sub6,
+                FBRX_Freq_Meas_sub6_Max,
+                FBRX_Freq_Meas_sub6_Min,
+                FBRX_Freq_Code_sub6,
+                FBRX_Freq_Code_sub6_Max,
+                FBRX_Freq_Code_sub6_Min,
+                PRX_Gain_2G,
+                Ripple_2G,
+                RXGain_3G,
+                RXComp_3G,
+                RXGain_sub6,
+                RXRSRP_sub6,
+                RXComp_sub6,
+                GMSK_Mean,
+                GMSK_TXL_Mean,
+                GMSK_Code_Mean,
+                EPSK_Mean,
+                EPSK_TXL_Mean,
+                EPSK_Code_Mean,
+                ETSAPT_3G_Psat_Ave,
+                ETSAPT_3G_Psat_Max,
+                ETSAPT_3G_Psat_Min,
+                ETSAPT_3G_Power_Ave,
+                ETSAPT_3G_Power_Max,
+                ETSAPT_3G_Power_Min,
+                thermistor,
+                APT_3G_Ave,
+                APT_Sub6_Ave,
+                APT_Sub6_Max,
+                APT_Sub6_Min,
+                ETSAPT_sub6_Psat_Ave,
+                ETSAPT_sub6_Psat_Max,
+                ETSAPT_sub6_Psat_Min,
+                ETSAPT_sub6_Freq_Ave,
+                ETSAPT_sub6_Freq_Max,
+                ETSAPT_sub6_Freq_Min,
+                ETSAPT_sub6_Pgain_Ave,
+                ETSAPT_sub6_Pgain_Max,
+                ETSAPT_sub6_Pgain_Min,
+                ETSAPT_sub6_Power_Ave,
+                ETSAPT_sub6_Power_Max,
+                ETSAPT_sub6_Power_Min,
+                BW_Cal,
+            )
+
     else:  # MTM
         for count, file in enumerate(file_list, start=1):
             Print_fname = os.path.basename(file)
@@ -215,157 +426,11 @@ def get_data(list_file, Select_op, Save_data_var, debug_var, raw_data_var, get_d
                 Result_Meas[f"Meas_{count}"] = Subset_Meas["Meas"].astype(float)
                 Result_Code[f"Code_{count}"] = Subset_Code["Code"].astype(float)
 
-    Result_Meas.dropna(how="all", axis=1, inplace=True)
-    Result_Code.dropna(how="all", axis=1, inplace=True)
+        Result_Meas.dropna(how="all", axis=1, inplace=True)
+        Result_Code.dropna(how="all", axis=1, inplace=True)
 
-    text_area.insert(tk.END, "=" * 85)
-    text_area.insert(tk.END, "\n")
-    text_area.see(tk.END)
-
-    if raw_data_var.get():
-        filename = "Result_Meas.xlsx"
-        with pd.ExcelWriter(filename) as writer:
-            Result_Meas.to_excel(writer, sheet_name="Meas_Full_Data")
-        wb = load_workbook(filename)
-        ws = wb.sheetnames
-        for sheet in ws:
-            col_max = wb[sheet].max_column
-            row_max = wb[sheet].max_row
-            text_area.insert(tk.END, f"{sheet:<30}| Col = {col_max:<5}, Row = {row_max:<5} | ")
-            text_area.see(tk.END)
-        text_area.insert(tk.END, f"Done\n")
+        text_area.insert(tk.END, "=" * 85)
+        text_area.insert(tk.END, "\n")
         text_area.see(tk.END)
 
-        filename = "Result_Code.xlsx"
-        with pd.ExcelWriter(filename) as writer:
-            Result_Code.to_excel(writer, sheet_name="Code_Full_Data")
-        wb = load_workbook(filename)
-        ws = wb.sheetnames
-        for sheet in ws:
-            col_max = wb[sheet].max_column
-            row_max = wb[sheet].max_row
-            text_area.insert(tk.END, f"{sheet:<30}| Col = {col_max:<5}, Row = {row_max:<5} | ")
-            text_area.see(tk.END)
-        text_area.insert(tk.END, f"Done\n")
-        text_area.see(tk.END)
-
-    if get_data_var.get():
-        Save_data = True
-    else:
-        Save_data = Save_data_var.get()
-
-    DC_Cal_Mean, IIP2_Cal_Mean, DC_Cal_3G, DC_Cal_2G, DC_Cal_NR = Ldciip2.daseul_dc_iip2(Result_Code, Save_data, text_area)
-    CableCheck = Lcable.daseul_cable_average(Result_Meas, Save_data, text_area)
-    RFIC_gain = Lget.rfic_gain_average(df_RFIC_gain, Save_data, text_area)
-    TxP_Channel_comp_ave = L3gtx.TxP_3g_channel_comp_pa_mid(Result_Meas, Save_data, text_area)
-    (
-        FBRX_Gain_Meas_3G,
-        FBRX_Gain_Meas_sub6,
-        FBRX_Gain_Code_3G,
-        FBRX_Gain_Code_sub6,
-        FBRX_Freq_Meas_3G,
-        FBRX_Freq_Meas_3G_Max,
-        FBRX_Freq_Meas_3G_Min,
-        FBRX_Freq_Meas_sub6,
-        FBRX_Freq_Meas_sub6_Max,
-        FBRX_Freq_Meas_sub6_Min,
-        FBRX_Freq_Code_sub6,
-        FBRX_Freq_Code_sub6_Max,
-        FBRX_Freq_Code_sub6_Min,
-    ) = Lfbrx.fbrx_average(Result_Meas, Result_Code, Save_data, text_area)
-    PRX_Gain_2G, Ripple_2G, RXGain_3G, RXComp_3G, RXGain_sub6, RXRSRP_sub6, RXComp_sub6 = Lrx_ave.daseul_rx_average(
-        Result_Meas, Save_data, text_area
-    )
-    GMSK_Mean, GMSK_TXL_Mean, GMSK_Code_Mean = L2gtx.gsm_tx_gmsk_average(Result_Meas, Result_Code, Save_data, text_area)
-    EPSK_Mean, EPSK_TXL_Mean, EPSK_Code_Mean = L2gtx.gsm_tx_edge_average(Result_Meas, Result_Code, Save_data, text_area)
-
-    (
-        ETSAPT_3G_Psat_Ave,
-        ETSAPT_3G_Psat_Max,
-        ETSAPT_3G_Psat_Min,
-        ETSAPT_3G_Power_Ave,
-        ETSAPT_3G_Power_Max,
-        ETSAPT_3G_Power_Min,
-    ) = L3get.Et_3g_average(Result_Meas, Save_data, text_area)
-
-    thermistor = Lsub6tx.therm_average(Result_Code, Save_data, text_area)
-
-    APT_3G_Ave, APT_Sub6_Ave, APT_Sub6_Max, APT_Sub6_Min = Lapt.apt_average(Result_Meas, Save_data, text_area)
-
-    (
-        ETSAPT_sub6_Psat_Ave,
-        ETSAPT_sub6_Psat_Max,
-        ETSAPT_sub6_Psat_Min,
-        ETSAPT_sub6_Freq_Ave,
-        ETSAPT_sub6_Freq_Max,
-        ETSAPT_sub6_Freq_Min,
-        ETSAPT_sub6_Pgain_Ave,
-        ETSAPT_sub6_Pgain_Max,
-        ETSAPT_sub6_Pgain_Min,
-        ETSAPT_sub6_Power_Ave,
-        ETSAPT_sub6_Power_Max,
-        ETSAPT_sub6_Power_Min,
-    ) = Lsub6et.sub6_et_average(Result_Meas, Save_data, text_area)
-    BW_Cal = Lsub6tx.sub6_bw_cal_average(Result_Meas, Save_data, text_area)
-
-    # ! Plotly
-    Ldraw.Drawing_dash(DC_Cal_Mean)
-
-    if get_data_var.get():
-        msgbox.showwarning("Message", "작업 완료")
-    else:
-        return (
-            CableCheck,
-            RFIC_gain,
-            TxP_Channel_comp_ave,
-            FBRX_Gain_Meas_3G,
-            FBRX_Gain_Meas_sub6,
-            FBRX_Gain_Code_3G,
-            FBRX_Gain_Code_sub6,
-            FBRX_Freq_Meas_3G,
-            FBRX_Freq_Meas_3G_Max,
-            FBRX_Freq_Meas_3G_Min,
-            FBRX_Freq_Meas_sub6,
-            FBRX_Freq_Meas_sub6_Max,
-            FBRX_Freq_Meas_sub6_Min,
-            FBRX_Freq_Code_sub6,
-            FBRX_Freq_Code_sub6_Max,
-            FBRX_Freq_Code_sub6_Min,
-            PRX_Gain_2G,
-            Ripple_2G,
-            RXGain_3G,
-            RXComp_3G,
-            RXGain_sub6,
-            RXRSRP_sub6,
-            RXComp_sub6,
-            GMSK_Mean,
-            GMSK_TXL_Mean,
-            GMSK_Code_Mean,
-            EPSK_Mean,
-            EPSK_TXL_Mean,
-            EPSK_Code_Mean,
-            ETSAPT_3G_Psat_Ave,
-            ETSAPT_3G_Psat_Max,
-            ETSAPT_3G_Psat_Min,
-            ETSAPT_3G_Power_Ave,
-            ETSAPT_3G_Power_Max,
-            ETSAPT_3G_Power_Min,
-            thermistor,
-            APT_3G_Ave,
-            APT_Sub6_Ave,
-            APT_Sub6_Max,
-            APT_Sub6_Min,
-            ETSAPT_sub6_Psat_Ave,
-            ETSAPT_sub6_Psat_Max,
-            ETSAPT_sub6_Psat_Min,
-            ETSAPT_sub6_Freq_Ave,
-            ETSAPT_sub6_Freq_Max,
-            ETSAPT_sub6_Freq_Min,
-            ETSAPT_sub6_Pgain_Ave,
-            ETSAPT_sub6_Pgain_Max,
-            ETSAPT_sub6_Pgain_Min,
-            ETSAPT_sub6_Power_Ave,
-            ETSAPT_sub6_Power_Max,
-            ETSAPT_sub6_Power_Min,
-            BW_Cal,
-        )
+        return Result_Meas
