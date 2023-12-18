ce12b4e2421566ab61f89aa092f332a790665047
DSROH <95155653+DSROH@users.noreply.github.com>
2023-12-18 22:13:21 +0900

    Update dash_app.py

--- Dash_Plotly/LSI_Solution/pages/dash_app.py
+++ Dash_Plotly/LSI_Solution/pages/dash_app.py
@@ -7,72 +7,16 @@ from LSI_Solution.pages.page_2g import initialize_2g
 from LSI_Solution.pages.page_3g import initialize_3g
 from LSI_Solution.pages.page_nr import initialize_nr
 
-# from pages import page_2G
 
-
-def MultiPageApp(
-    df_TXDC,
-    df_IIP2,
-    df_Cable,
-    df_3GTXCP,
-    df_fbrxgm_3G,
-    df_fbrxgc_3G,
-    df_fbrxfm_3G,
-    df_fbrxfm_3G_ch,
-    df_fbrxgm_NR,
-    df_fbrxgc_NR,
-    df_fbrxfm_NR,
-    df_fbrxfc_NR,
-    df_PRX_Gain_2G,
-    df_Ripple_2G,
-    df_RXGain_3G,
-    df_RXComp_3G,
-    df_RXGain_sub6,
-    df_RXRSRP_sub6,
-    df_RXComp_sub6,
-    df_RXMixer_sub6,
-    df_APT_Meas_3G,
-    df_APT_Meas_sub6,
-    df_3G_ETSAPT_Pst,
-    df_3G_ETSAPT_Power,
-    df_NR_ETSAPT_Psat,
-    df_NR_ETSAPT_Pgain,
-    df_NR_ETSAPT_Power,
-    df_NR_ETSAPT_Freq,
-):
+def MultiPageApp(dict_cf, dict_2g, dict_3g, dict_nr):
     # app = dash.Dash(__name__, use_pages=True, pages_folder=pages_folder, external_stylesheets=[dbc.themes.COSMO])
     app = dash.Dash(__name__, use_pages=True, pages_folder="", external_stylesheets=[dbc.themes.COSMO])
     # app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.COSMO])
 
-    initialize_cf(df_TXDC, df_IIP2, df_Cable)
-    initialize_2g(df_PRX_Gain_2G, df_Ripple_2G)
-    initialize_3g(
-        df_3GTXCP,
-        df_fbrxgm_3G,
-        df_fbrxgc_3G,
-        df_fbrxfm_3G,
-        df_fbrxfm_3G_ch,
-        df_RXGain_3G,
-        df_RXComp_3G,
-        df_APT_Meas_3G,
-        df_3G_ETSAPT_Pst,
-        df_3G_ETSAPT_Power,
-    )
-    initialize_nr(
-        df_fbrxgm_NR,
-        df_fbrxgc_NR,
-        df_fbrxfm_NR,
-        df_fbrxfc_NR,
-        df_RXGain_sub6,
-        df_RXRSRP_sub6,
-        df_RXComp_sub6,
-        df_RXMixer_sub6,
-        df_APT_Meas_sub6,
-        df_NR_ETSAPT_Psat,
-        df_NR_ETSAPT_Pgain,
-        df_NR_ETSAPT_Power,
-        df_NR_ETSAPT_Freq,
-    )
+    initialize_cf(dict_cf)
+    initialize_2g(dict_2g)
+    initialize_3g(dict_3g)
+    initialize_nr(dict_nr)
 
     sidebar = dbc.Nav(
         [
@@ -92,7 +36,13 @@ def MultiPageApp(
 
     app.layout = dbc.Container(
         [
-            dbc.Row([dbc.Col(html.Div("Dashboard [Model name] RF Cal Data", style={"fontSize": 50, "textAlign": "center"}))]),
+            dbc.Row(
+                [
+                    dbc.Col(
+                        html.Div("Dashboard [Model name] RF Cal Data", style={"fontSize": 50, "textAlign": "center"})
+                    )
+                ]
+            ),
             html.Hr(),
             dbc.Row(
                 [
