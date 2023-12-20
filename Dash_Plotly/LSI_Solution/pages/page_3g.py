5ad1a9ba370a3c4ec9aaee383741ffbe5c995251
DSROH <95155653+DSROH@users.noreply.github.com>
2023-12-20 20:20:36 +0900

    Update page_3g.py

--- Dash_Plotly/LSI_Solution/pages/page_3g.py
+++ Dash_Plotly/LSI_Solution/pages/page_3g.py
@@ -1,7 +1,7 @@
 import dash
 import dash_bootstrap_components as dbc
 from dash import dcc, html, callback, Output, Input
-from LSI_Solution.pages.page_cf import create_dropdown, create_range_slider, Initialize_band, update_band_and_graph
+from LSI_Solution.pages.page_cf import create_dropdown, create_range_slider, Initialize_band, Band_list, update_band_and_graph
 
 
 def initialize_3g(dict_3g):
@@ -295,8 +295,8 @@ def initialize_3g(dict_3g):
 
     # ? 3G TxP Channel Components
     @callback(Output("3G_TXCP_band", "value"), Input("3G_TXCP_RAT", "value"))
-    def TXCP_3G(Sel_rat):
-        return Initialize_band(Sel_rat, df_TXCP)
+    def TXCP_3G(selected_rat):
+        return Initialize_band(selected_rat, df_TXCP)
 
     @callback(
         [
@@ -306,14 +306,16 @@ def initialize_3g(dict_3g):
         ],
         [Input("3G_TXCP_RAT", "value"), Input("3G_TXCP_band", "value"), Input("sld_TXCP_scat", "value")],
     )
-    def update_TXCP(Sel_rat, Sel_band, scatt_range):
-        band_opt, scatter_fig, histogram_fig = update_band_and_graph(df_TXCP, Sel_rat, Sel_band, scatt_range)
+    def update_TXCP(selected_rat, selected_band, scatt_range):
+        scatter_fig, histogram_fig = update_band_and_graph(df_TXCP, selected_rat, selected_band, scatt_range)
+        band_opt = Band_list(df_TXCP, selected_rat)
+
         return band_opt, scatter_fig, histogram_fig
 
     # ? 3G FBRX Gain Cal
     @callback(Output("3G_FBRXGain_band", "value"), Input("3G_FBRXGain_RAT", "value"))
-    def FBRXGain_3G(Sel_rat):
-        return Initialize_band(Sel_rat, df_fbrxgm)
+    def FBRXGain_3G(selected_rat):
+        return Initialize_band(selected_rat, df_fbrxgm)
 
     @callback(
         [
@@ -330,15 +332,17 @@ def initialize_3g(dict_3g):
             Input("sld_FBRX_GC_scat", "value"),
         ],
     )
-    def update_FBRXGain(Sel_rat, Sel_band, scatt_range1, scatt_range2):
-        band_opt, scatt_fig1, histo_fig1 = update_band_and_graph(df_fbrxgm, Sel_rat, Sel_band, scatt_range1)
-        band_opt, scatt_fig2, histo_fig2 = update_band_and_graph(df_fbrxgc, Sel_rat, Sel_band, scatt_range2)
+    def update_FBRXGain(selected_rat, selected_band, scatt_range1, scatt_range2):
+        scatt_fig1, histo_fig1 = update_band_and_graph(df_fbrxgm, selected_rat, selected_band, scatt_range1)
+        scatt_fig2, histo_fig2 = update_band_and_graph(df_fbrxgc, selected_rat, selected_band, scatt_range2)
+        band_opt = Band_list(df_fbrxgm, selected_rat)
+
         return band_opt, scatt_fig1, histo_fig1, scatt_fig2, histo_fig2
 
     # ? 3G FBRX Freq Cal
     @callback(Output("3G_FBRXFreq_band", "value"), Input("3G_FBRXFreq_RAT", "value"))
-    def FBRXFreq_3G(Sel_rat):
-        return Initialize_band(Sel_rat, df_fbrxfm)
+    def FBRXFreq_3G(selected_rat):
+        return Initialize_band(selected_rat, df_fbrxfm)
 
     @callback(
         [
@@ -348,14 +352,16 @@ def initialize_3g(dict_3g):
         ],
         [Input("3G_FBRXFreq_RAT", "value"), Input("3G_FBRXFreq_band", "value"), Input("sld_FBRX_FM_scat", "value")],
     )
-    def update_FBRXFreq(Sel_rat, Sel_band, scatt_range):
-        band_opt, scatter_fig, histogram_fig = update_band_and_graph(df_fbrxfm, Sel_rat, Sel_band, scatt_range)
+    def update_FBRXFreq(selected_rat, selected_band, scatt_range):
+        scatter_fig, histogram_fig = update_band_and_graph(df_fbrxfm, selected_rat, selected_band, scatt_range)
+        band_opt = Band_list(df_fbrxfm, selected_rat)
+
         return band_opt, scatter_fig, histogram_fig
 
     # ? 3G FBRX Freq Cal - Channel
     # @callback(Output("3G_FBRXFreq_ch_band", "value"), Input("3G_FBRXFreq_ch_RAT", "value"))
-    # def FBRXFreq_Ch_3G(Sel_rat):
-    #     return Initialize_band(Sel_rat, df_fbrxfm_ch)
+    # def FBRXFreq_Ch_3G(selected_rat):
+    #     return Initialize_band(selected_rat, df_fbrxfm_ch)
 
     # @callback(
     #     [
@@ -365,14 +371,16 @@ def initialize_3g(dict_3g):
     #     ],
     #     [Input("3G_FBRXFreq_ch_RAT", "value"), Input("3G_FBRXFreq_ch_band", "value")],
     # )
-    # def update_FBRXFreq_ch(Sel_rat, Sel_band):
-    #     band_opt, scatter_fig1, scatter_fig2 = update_band_and_graph(df_fbrxfm_ch, Sel_rat, Sel_band, None)
+    # def update_FBRXFreq_ch(selected_rat, selected_band):
+    #     scatter_fig1, scatter_fig2 = update_band_and_graph(df_fbrxfm_ch, selected_rat, selected_band, None)
+    #     band_opt = Band_list(df_fbrxfm_ch)
+
     #     return band_opt, scatter_fig1, scatter_fig2
 
     # ? 3G RX Gain Cal
     @callback(Output("3G_RXGain_band", "value"), Input("3G_RXGain_RAT", "value"))
-    def RXGain_3G(Sel_rat):
-        return Initialize_band(Sel_rat, df_RXGain)
+    def RXGain_3G(selected_rat):
+        return Initialize_band(selected_rat, df_RXGain)
 
     @callback(
         [
@@ -382,14 +390,16 @@ def initialize_3g(dict_3g):
         ],
         [Input("3G_RXGain_RAT", "value"), Input("3G_RXGain_band", "value"), Input("sld_RXGain_scat", "value")],
     )
-    def update_RXGain(Sel_rat, Sel_band, scatt_range):
-        band_opt, scatter_fig, histogram_fig = update_band_and_graph(df_RXGain, Sel_rat, Sel_band, scatt_range)
+    def update_RXGain(selected_rat, selected_band, scatt_range):
+        scatter_fig, histogram_fig = update_band_and_graph(df_RXGain, selected_rat, selected_band, scatt_range)
+        band_opt = Band_list(df_RXGain, selected_rat)
+
         return band_opt, scatter_fig, histogram_fig
 
     # ? 3G RX Channel components
     @callback(Output("3G_RXComp_band", "value"), Input("3G_RXComp_RAT", "value"))
-    def RXComp_3G(Sel_rat):
-        return Initialize_band(Sel_rat, df_RXComp)
+    def RXComp_3G(selected_rat):
+        return Initialize_band(selected_rat, df_RXComp)
 
     @callback(
         [
@@ -399,16 +409,18 @@ def initialize_3g(dict_3g):
         ],
         [Input("3G_RXComp_RAT", "value"), Input("3G_RXComp_band", "value"), Input("sld_RXComp_scat", "value")],
     )
-    def update_RXComp(Sel_rat, Sel_band, scatt_range):
-        band_opt, scatter_fig, histogram_fig = update_band_and_graph(df_RXComp, Sel_rat, Sel_band, scatt_range)
+    def update_RXComp(selected_rat, selected_band, scatt_range):
+        scatter_fig, histogram_fig = update_band_and_graph(df_RXComp, selected_rat, selected_band, scatt_range)
+        band_opt = Band_list(df_RXComp, selected_rat)
+
         return band_opt, scatter_fig, histogram_fig
 
     dash.register_page(__name__, path="/WCDMA", name="WCDMA", title="WCDMA", layout=layout)
 
     # ? 3G APT Measuremnet
     @callback(Output("3G_APTMeas_band", "value"), Input("3G_APTMeas_RAT", "value"))
-    def APT_Meas_3G(Sel_rat):
-        return Initialize_band(Sel_rat, df_APT_Meas)
+    def APT_Meas_3G(selected_rat):
+        return Initialize_band(selected_rat, df_APT_Meas)
 
     @callback(
         [
@@ -418,14 +430,16 @@ def initialize_3g(dict_3g):
         ],
         [Input("3G_APTMeas_RAT", "value"), Input("3G_APTMeas_band", "value"), Input("sld_APTMeas_scat", "value")],
     )
-    def update_APT_Meas(Sel_rat, Sel_band, scatt_range):
-        band_opt, scatter_fig, histogram_fig = update_band_and_graph(df_APT_Meas, Sel_rat, Sel_band, scatt_range)
+    def update_APT_Meas(selected_rat, selected_band, scatt_range):
+        scatter_fig, histogram_fig = update_band_and_graph(df_APT_Meas, selected_rat, selected_band, scatt_range)
+        band_opt = Band_list(df_APT_Meas, selected_rat)
+
         return band_opt, scatter_fig, histogram_fig
 
     # ? 3G ET Psat
     @callback(Output("3G_ETPsat_band", "value"), Input("3G_ETPsat_RAT", "value"))
-    def ETPsat_3G(Sel_rat):
-        return Initialize_band(Sel_rat, df_ET_Pst)
+    def ETPsat_3G(selected_rat):
+        return Initialize_band(selected_rat, df_ET_Pst)
 
     @callback(
         [
@@ -435,14 +449,16 @@ def initialize_3g(dict_3g):
         ],
         [Input("3G_ETPsat_RAT", "value"), Input("3G_ETPsat_band", "value"), Input("sld_ETPsat_scat", "value")],
     )
-    def update_ETPsat(Sel_rat, Sel_band, scatt_range):
-        band_opt, scatter_fig, histogram_fig = update_band_and_graph(df_ET_Pst, Sel_rat, Sel_band, scatt_range)
+    def update_ETPsat(selected_rat, selected_band, scatt_range):
+        scatter_fig, histogram_fig = update_band_and_graph(df_ET_Pst, selected_rat, selected_band, scatt_range)
+        band_opt = Band_list(df_ET_Pst, selected_rat)
+
         return band_opt, scatter_fig, histogram_fig
 
     # ? 3G ET Power
     @callback(Output("3G_ETPower_band", "value"), Input("3G_ETPower_RAT", "value"))
-    def ETPower_3G(Sel_rat):
-        return Initialize_band(Sel_rat, df_ET_Power)
+    def ETPower_3G(selected_rat):
+        return Initialize_band(selected_rat, df_ET_Power)
 
     @callback(
         [
@@ -452,8 +468,10 @@ def initialize_3g(dict_3g):
         ],
         [Input("3G_ETPower_RAT", "value"), Input("3G_ETPower_band", "value"), Input("sld_ETPower_scat", "value")],
     )
-    def update_ETPower(Sel_rat, Sel_band, scatt_range):
-        band_opt, scatter_fig, histogram_fig = update_band_and_graph(df_ET_Power, Sel_rat, Sel_band, scatt_range)
+    def update_ETPower(selected_rat, selected_band, scatt_range):
+        scatter_fig, histogram_fig = update_band_and_graph(df_ET_Power, selected_rat, selected_band, scatt_range)
+        band_opt = Band_list(df_ET_Power, selected_rat)
+
         return band_opt, scatter_fig, histogram_fig
 
     dash.register_page(__name__, path="/WCDMA", name="WCDMA", title="WCDMA", layout=layout)
