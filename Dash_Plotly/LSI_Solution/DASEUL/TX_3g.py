bf46d65dad7408d41dc2c00dd97952000a8b47d9
DSROH <95155653+DSROH@users.noreply.github.com>
2023-12-29 12:02:40 +0900

    Update TX_3g.py

--- Dash_Plotly/LSI_Solution/DASEUL/TX_3g.py
+++ Dash_Plotly/LSI_Solution/DASEUL/TX_3g.py
@@ -69,7 +69,8 @@ def chng_3g_rfic_gain(Selected_spc, rat, band, RFIC_Spec_var, RFIC_gain, text_ar
     f.close()
 
 
-def chng_3g_fbrx_gain_meas(Selected_spc, rat, band, FBRX_3G_Spec_var, FBRX_Gain_Meas_3G, text_area):
+def chng_3g_fbrx_gain_meas(Selected_spc, rat, band, FBRX_3G_Spec_var, df_FBRX_Gain_Meas_3G, text_area):
+    FBRX_Gain_Meas_3G = df_FBRX_Gain_Meas_3G["Average"]
     FBRX_Spec = int(FBRX_3G_Spec_var.get())  # 1dB = 100
     target_word = f"[{rat}_BAND{band}_Calibration_Spec]"
     band = f"B{band}"
@@ -127,7 +128,8 @@ def chng_3g_fbrx_gain_meas(Selected_spc, rat, band, FBRX_3G_Spec_var, FBRX_Gain_
     f.close()
 
 
-def chng_3g_fbrx_gain_code(Selected_spc, rat, band, FBRX_3G_Spec_var, FBRX_Gain_Code_3G, text_area):
+def chng_3g_fbrx_gain_code(Selected_spc, rat, band, FBRX_3G_Spec_var, df_FBRX_Gain_Code_3G, text_area):
+    FBRX_Gain_Code_3G = df_FBRX_Gain_Code_3G["Average"]
     FBRX_Spec = int(FBRX_3G_Spec_var.get()) * 100  # 1dB = 100
     target_word = f"[{rat}_BAND{band}_Calibration_Spec]"
     band = f"B{band}"
@@ -174,16 +176,10 @@ def chng_3g_fbrx_gain_code(Selected_spc, rat, band, FBRX_3G_Spec_var, FBRX_Gain_
     f.close()
 
 
-def chng_3g_fbrx_freq_meas(
-    Selected_spc,
-    rat,
-    band,
-    FBRX_3G_Spec_var,
-    FBRX_Freq_Meas_3G,
-    FBRX_Freq_Meas_3G_Max,
-    FBRX_Freq_Meas_3G_Min,
-    text_area,
-):
+def chng_3g_fbrx_freq_meas(Selected_spc, rat, band, FBRX_3G_Spec_var, df_FBRX_Freq_Meas_3G, text_area):
+    FBRX_Freq_Meas_3G = df_FBRX_Freq_Meas_3G["Average"]
+    FBRX_Freq_Meas_3G_Max = df_FBRX_Freq_Meas_3G["Max"]
+    FBRX_Freq_Meas_3G_Min = df_FBRX_Freq_Meas_3G["Min"]
     FBRX_Spec = int(FBRX_3G_Spec_var.get())  # 1dB = 100
     target_word = f"[{rat}_BAND{band}_Calibration_Spec]"
     band = f"B{band}"
@@ -206,7 +202,7 @@ def chng_3g_fbrx_freq_meas(
                 tk.END,
                 f"{New_String[0]:<30}| {New_String[3]:>5}\t{New_String[4]:>5}\t\t\u2192\t",
             )
-            New_String[2] = round(FBRX_Freq_Meas_3G["WCDMA", band])
+            New_String[2] = round(FBRX_Freq_Meas_3G["WCDMA", band, "Tx", "FBRX"])
             New_String[3] = New_String[2] - FBRX_Spec
             New_String[4] = New_String[2] + FBRX_Spec
             text_area.insert(tk.END, f"{New_String[3]:>5}\t{New_String[4]:>5}\n")
@@ -223,7 +219,9 @@ def chng_3g_fbrx_freq_meas(
                 tk.END,
                 f"{New_String[0]:<30}| {New_String[3]:>5}\t{New_String[4]:>5}\t\t\u2192\t",
             )
-            New_String[2] = round(FBRX_Freq_Meas_3G_Max["WCDMA", band]) - round(FBRX_Freq_Meas_3G_Min["WCDMA", band])
+            New_String[2] = round(FBRX_Freq_Meas_3G_Max["WCDMA", band, "Tx", "FBRX"]) - round(
+                FBRX_Freq_Meas_3G_Min["WCDMA", band, "Tx", "FBRX"]
+            )
             New_String[3] = 0
             New_String[4] = New_String[2] + 3
             text_area.insert(tk.END, f"{New_String[3]:>5}\t{New_String[4]:>5}\n")
@@ -246,7 +244,8 @@ def chng_3g_fbrx_freq_meas(
     f.close()
 
 
-def chng_Txp_comp(Selected_spc, rat, band, TxP_Channel_comp_Spec_var, TxP_Channel_comp_ave):
+def chng_Txp_comp(Selected_spc, rat, band, TxP_Channel_comp_Spec_var, dF_TxPCh_Comp_ave):
+    TxP_Channel_comp_ave = dF_TxPCh_Comp_ave["Average"]
     target_word = f"[{rat}_BAND{band}_Calibration_Spec]"
     band = f"B{band}"
     new_text_content = ""
@@ -282,12 +281,12 @@ def Txp_channel_comp(line, band, TxP_Channel_comp_Spec_var, TxP_Channel_comp_ave
     Word = re.split("_", New_String[0])
     Word = [v for v in Word if v]
     Read_index = int(Word[5])
-    Index_count = TxP_Channel_comp_ave.loc[(band)].count()
+    Index_count = TxP_Channel_comp_ave.loc[("WCDMA", band, "Tx")].count()
 
     if Read_index < Index_count:
-        New_String[1] = round(TxP_Channel_comp_ave[band].iloc[Read_index])
-        New_String[2] = round(TxP_Channel_comp_ave[band].iloc[Read_index]) - TxP_Channel_comp_Spec_var
-        New_String[3] = round(TxP_Channel_comp_ave[band].iloc[Read_index]) + TxP_Channel_comp_Spec_var
+        New_String[1] = round(TxP_Channel_comp_ave["WCDMA", band, "Tx"].iloc[Read_index])
+        New_String[2] = round(TxP_Channel_comp_ave["WCDMA", band, "Tx"].iloc[Read_index]) - TxP_Channel_comp_Spec_var
+        New_String[3] = round(TxP_Channel_comp_ave["WCDMA", band, "Tx"].iloc[Read_index]) + TxP_Channel_comp_Spec_var
     else:
         New_String[1] = 0
         New_String[2] = New_String[1] - TxP_Channel_comp_Spec_var
@@ -300,31 +299,38 @@ def Txp_channel_comp(line, band, TxP_Channel_comp_Spec_var, TxP_Channel_comp_ave
     return New_String
 
 
-def TxP_3g_channel_comp_pa_mid(df_Meas, Save_data, text_area):
-    df_TxP_Channel_comp = df_Meas[df_Meas["Test Conditions"].str.contains("_PA_MID_Comp ").to_list()]
-    df_TxP_Channel_comp_Value = df_TxP_Channel_comp.iloc[:, 1:].astype(float)
-    df_TxP_Channel_comp_Item = df_TxP_Channel_comp["Test Conditions"].str.split("_|\\(|\\)", expand=True)
+def Txp_cc_3g(df_Meas, Save_data, text_area):
+    df_TxPCh_Comp = df_Meas[df_Meas["Test Conditions"].str.contains("_PA_MID_Comp ").to_list()]
+    df_TxPCh_Comp_Value = df_TxPCh_Comp.iloc[:, 1:].astype(float)
+    df_TxPCh_Comp_Item = df_TxPCh_Comp["Test Conditions"].str.split("_|\\(|\\)", expand=True)
     # 의미없는 컬럼 삭제
-    df_TxP_Channel_comp_Item.drop(columns=[0, 2, 4, 5, 6, 7, 8], inplace=True)
+    df_TxPCh_Comp_Item.drop(columns=[4, 5, 6, 7, 8], inplace=True)
     # groupby 실행을 위한 컬럼명 변경
-    df_TxP_Channel_comp_Item.columns = ["Band", "Channel"]
-    df_TxP_Channel_comp = pd.merge(df_TxP_Channel_comp_Item, df_TxP_Channel_comp_Value, left_index=True, right_index=True)
-    df_TxP_Channel_comp_mean = round(df_TxP_Channel_comp.groupby(["Band", "Channel"], sort=False).mean(), 2)
+    df_TxPCh_Comp_Item.columns = ["RAT", "Band", "Path", "CH"]
+    df_TxPCh_Comp = pd.merge(df_TxPCh_Comp_Item, df_TxPCh_Comp_Value, left_index=True, right_index=True)
+    df_TxPCh_Comp_mean = round(df_TxPCh_Comp.groupby(["RAT", "Band", "Path", "CH"], sort=False).mean(), 2)
+    df_TxPCh_Comp_max = round(df_TxPCh_Comp.groupby(["RAT", "Band", "Path", "CH"], sort=False).max(), 2)
+    df_TxPCh_Comp_min = round(df_TxPCh_Comp.groupby(["RAT", "Band", "Path", "CH"], sort=False).min(), 2)
 
-    df_TxP_Channel_comp_mean = pd.concat(
-        [df_TxP_Channel_comp_mean, round(df_TxP_Channel_comp_mean.mean(axis=1), 2).rename("Average")], axis=1
-    )
-    df_TxP_Channel_comp_mean = pd.concat(
-        [df_TxP_Channel_comp_mean, round(df_TxP_Channel_comp_mean.max(axis=1), 2).rename("Max")], axis=1
-    )
-    df_TxP_Channel_comp_mean = pd.concat(
-        [df_TxP_Channel_comp_mean, round(df_TxP_Channel_comp_mean.min(axis=1), 2).rename("Min")], axis=1
-    )
+    df_TxPCh_Comp_mean = pd.concat([df_TxPCh_Comp_mean, round(df_TxPCh_Comp_mean.mean(axis=1), 2).rename("Average")], axis=1)
+    df_TxPCh_Comp_mean = pd.concat([df_TxPCh_Comp_mean, round(df_TxPCh_Comp_max.max(axis=1), 2).rename("Max")], axis=1)
+    df_TxPCh_Comp_mean = pd.concat([df_TxPCh_Comp_mean, round(df_TxPCh_Comp_min.min(axis=1), 2).rename("Min")], axis=1)
+    df_TxPCh_Comp_max = pd.concat([df_TxPCh_Comp_max, round(df_TxPCh_Comp_max.mean(axis=1), 2).rename("Average")], axis=1)
+    df_TxPCh_Comp_max = pd.concat([df_TxPCh_Comp_max, round(df_TxPCh_Comp_max.max(axis=1), 2).rename("Max")], axis=1)
+    df_TxPCh_Comp_max = pd.concat([df_TxPCh_Comp_max, round(df_TxPCh_Comp_max.min(axis=1), 2).rename("Min")], axis=1)
+    df_TxPCh_Comp_min = pd.concat([df_TxPCh_Comp_min, round(df_TxPCh_Comp_min.mean(axis=1), 2).rename("Average")], axis=1)
+    df_TxPCh_Comp_min = pd.concat([df_TxPCh_Comp_min, round(df_TxPCh_Comp_min.max(axis=1), 2).rename("Max")], axis=1)
+    df_TxPCh_Comp_min = pd.concat([df_TxPCh_Comp_min, round(df_TxPCh_Comp_min.min(axis=1), 2).rename("Min")], axis=1)
 
     if Save_data:
-        filename = "Excel_3G_TxP_Channel_Comp.xlsx"
+        filename = "Excel_TxP_CC_3G.xlsx"
         with pd.ExcelWriter(filename) as writer:
-            df_TxP_Channel_comp_mean.to_excel(writer, sheet_name="3G_TxP_Channel_Comp_Mean")
-        func.WB_Format(filename, 2, 2, 0, text_area)
+            df_TxPCh_Comp_mean.to_excel(writer, sheet_name="3G_TxP_Channel_Comp_Mean")
+            df_TxPCh_Comp_max.to_excel(writer, sheet_name="3G_TxP_Channel_Comp_Max")
+            df_TxPCh_Comp_min.to_excel(writer, sheet_name="3G_TxP_Channel_Comp_Min")
+        func.WB_Format(filename, 2, 5, 0, text_area)
 
-    return df_TxP_Channel_comp_mean["Average"]
+        df_TxPCh_Comp_mean.to_csv("CSV_TxP_CC_3G.csv", encoding="utf-8-sig")
+
+    return df_TxPCh_Comp_mean
+
