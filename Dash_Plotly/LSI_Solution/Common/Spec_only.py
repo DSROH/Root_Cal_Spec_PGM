e40de1239d4b7b8e74a7e4a04c262eb328bdecc4
DSROH <95155653+DSROH@users.noreply.github.com>
2025-01-03 19:09:14 +0900

    2025-01-03

--- Dash_Plotly/LSI_Solution/Common/Spec_only.py
+++ Dash_Plotly/LSI_Solution/Common/Spec_only.py
@@ -1,6 +1,7 @@
 import re
+import math
 import tkinter as tk
-import LSI_Solution.DASEUL.RX_2g as L2grx
+import LSI_Solution.DASEUL.Root.RX_2g as root_2grx
 import numpy as np
 
 
@@ -123,7 +124,8 @@ def change_rx_spec(line, RX_Gain_Spec, text_area):
     String = [v for v in String if v]
     if String[1] == "RsrpOffset":
         text_area.insert(
-            tk.END, f"{String[0]:^4} RSRP   {String[2]:<6} {String[3]:<1}          | {String[5]:>5}\t{String[6]:>5}\t  \u2192  "
+            tk.END,
+            f"{String[0]:^4} RSRP   {String[2]:<6} {String[3]:<1}          | {String[5]:>5}\t{String[6]:>5}\t  \u2192  ",
         )
     else:
         text_area.insert(
@@ -190,28 +192,28 @@ def Chng_2G_rx_gain_spec_only(Selected_spc, rat, band, RX_Gain_2G_Spec_var, text
             String = Old_String = line
             String = re.split("\t|\n", line)
             String = [v for v in String if v]
-            New_String = L2grx.rx_gain(PRX_Gain_2G, RX_Gain_2G_Spec, band, "Spec_Only", String, text_area)
+            New_String = root_2grx.rx_gain(PRX_Gain_2G, RX_Gain_2G_Spec, band, "Spec_Only", String, "None", text_area)
             Change_Str = line.replace(Old_String, New_String)
             new_text_content += Change_Str
         elif Check & line.startswith("Rx_AGCOffset_1"):
             String = Old_String = line
             String = re.split("\t|\n", line)
             String = [v for v in String if v]
-            New_String = L2grx.rx_gain(PRX_Gain_2G, RX_Gain_2G_Spec, band, "Spec_Only", String, text_area)
+            New_String = root_2grx.rx_gain(PRX_Gain_2G, RX_Gain_2G_Spec, band, "Spec_Only", String, "None", text_area)
             Change_Str = line.replace(Old_String, New_String)
             new_text_content += Change_Str
         elif Check & line.startswith("Rx_AGCOffset_2"):
             String = Old_String = line
             String = re.split("\t|\n", line)
             String = [v for v in String if v]
-            New_String = L2grx.rx_gain(PRX_Gain_2G, RX_Gain_2G_Spec, band, "Spec_Only", String, text_area)
+            New_String = root_2grx.rx_gain(PRX_Gain_2G, RX_Gain_2G_Spec, band, "Spec_Only", String, "None", text_area)
             Change_Str = line.replace(Old_String, New_String)
             new_text_content += Change_Str
         elif Check & line.startswith("Rx_AGCOffset_3"):
             String = Old_String = line
             String = re.split("\t|\n", line)
             String = [v for v in String if v]
-            New_String = L2grx.rx_gain(PRX_Gain_2G, RX_Gain_2G_Spec, band, "Spec_Only", String, text_area)
+            New_String = root_2grx.rx_gain(PRX_Gain_2G, RX_Gain_2G_Spec, band, "Spec_Only", String, "None", text_area)
             Change_Str = line.replace(Old_String, New_String)
             new_text_content += Change_Str
         elif Check and line.startswith("GMSK_Ref_Power"):
@@ -226,7 +228,15 @@ def Chng_2G_rx_gain_spec_only(Selected_spc, rat, band, RX_Gain_2G_Spec_var, text
 
 
 def Chng_2g_tx_spec_only(
-    Selected_spc, band, GMSK_Spec_var, GTxL_Spec_var, GCode_Spec_var, EPSK_Spec_var, ETxL_Spec_var, ECode_Spec_var, text_area
+    Selected_spc,
+    band,
+    GMSK_Spec_var,
+    GTxL_Spec_var,
+    GCode_Spec_var,
+    EPSK_Spec_var,
+    ETxL_Spec_var,
+    ECode_Spec_var,
+    text_area,
 ):
     # 2G TX FBRX Spec
     GMSK_2G_Spec = float(GMSK_Spec_var.get())
@@ -565,8 +575,9 @@ def apt_spec_only(line, APT_Spec):
     if (New_String[1] == "-10") & (New_String[2] == "-10.1") & (New_String[3] == "-9.9"):
         pass
     else:
-        New_String[2] = int(New_String[1]) - float(APT_Spec)
-        New_String[3] = int(New_String[1]) + float(APT_Spec)
+        New_String[1] = math.floor(float(New_String[1]) + 0.5)
+        New_String[2] = New_String[1] - float(APT_Spec)
+        New_String[3] = New_String[1] + float(APT_Spec)
 
     New_String[0] = "_".join(Word) + "\t" + "="
     New_String = [str(v) for v in New_String]
@@ -581,10 +592,335 @@ def sub6_apt_slim_spec_only(line, APT_Spec):
     Word = re.split("_", New_String[0])
     Word = [v for v in Word if v]
     New_String[1] = 0
-    New_String[2] = int(New_String[1]) - float(APT_Spec)
-    New_String[3] = int(New_String[1]) + float(APT_Spec)
+    New_String[2] = New_String[1] - float(APT_Spec)
+    New_String[3] = New_String[1] + float(APT_Spec)
     New_String[0] = "_".join(Word) + "\t" + "="
     New_String = [str(v) for v in New_String]
     New_String = "\t".join(New_String) + "\n"
+
+    return New_String
+
+
+def Chng_rx_gain_spec_only_Slm(Selected_spc, rat, band, RX_Gain_Spec_var, text_area):
+    RX_Gain_Spec = float(RX_Gain_Spec_var.get())
+    new_text_content = ""
+    Check = False
+
+    if rat == "SUB6":
+        Search_WD = f"[{rat}_n{band}_Calibration_Spec]"
+
+    with open(Selected_spc, "r", encoding="utf-8") as file:
+        data_lines = file.readlines()
+    file.close()
+
+    for index, line in enumerate(data_lines):
+        New_String = Old_String = line
+        if line.startswith(Search_WD):
+            new_text_content += line
+            Check = True
+            text_area.insert(tk.END, f"\n\n{Search_WD}\n\n")
+            # Gainstate를 for로 할 경우 너무 많은 반복 -> if 문으로 처리
+        elif Check and line.startswith("RX_GAIN_Port"):
+            New_String = change_rx_spec_Slm(line, RX_Gain_Spec, text_area)
+            Change_Str = line.replace(Old_String, New_String)
+            new_text_content += Change_Str
+        elif Check and line.startswith("RX_RSSI_Offset_P"):
+            New_String = change_rx_ch_spec_Slm(line, RX_Gain_Spec, text_area)
+            Change_Str = line.replace(Old_String, New_String)
+            new_text_content += Change_Str
+        elif Check and line.startswith("RX_FREQ_Offset_P"):
+            new_text_content += line
+            Check = False
+        else:
+            new_text_content += line
+
+    with open(Selected_spc, "w", encoding="utf-8") as f:
+        f.writelines(new_text_content)
+    f.close()
+
+
+def change_rx_spec_Slm(line, RX_Gain_Spec, text_area):
+    String = [v for v in re.split("[=,\t\n]", line) if v]
+    text_area.insert(
+        tk.END,
+        f"{String[0]:<30} | {String[2]:>5}\t{String[3]:>5}\t  \u2192  ",
+    )
+    RX_Gain_Value = [float(String[2]), float(String[3])]
+    RX_Gain_mean = np.mean(RX_Gain_Value)
+
+    String[1] = round(RX_Gain_mean)
+    String[2] = round(RX_Gain_mean - RX_Gain_Spec)
+    String[3] = round(RX_Gain_mean + RX_Gain_Spec)
+
+    text_area.insert(tk.END, f"\t{String[2]:>5}\t{String[3]:>5}\n")
+    text_area.see(tk.END)
+    New_String = "\t".join(map(str, String[1:]))
+    New_String = String[0] + "\t=\t" + New_String + "\n"
+
+    return New_String
+
+
+def change_rx_ch_spec_Slm(line, RX_ch_Spec, text_area):
+    String = [v for v in re.split("[=,\t\n]", line) if v]
+    text_area.insert(
+        tk.END,
+        f"{String[0]:<30} | {String[2]:>5}\t{String[3]:>5}\t  \u2192  ",
+    )
+    RX_Gain_Value = [float(String[2]), float(String[3])]
+    RX_Gain_mean = np.mean(RX_Gain_Value)
+
+    String[1] = round(RX_Gain_mean)
+    String[2] = round(RX_Gain_mean - RX_ch_Spec)
+    String[3] = round(RX_Gain_mean + RX_ch_Spec)
+
+    text_area.insert(tk.END, f"\t{String[2]:>5}\t{String[3]:>5}\n")
+    text_area.see(tk.END)
+    New_String = "\t".join(map(str, String[1:]))
+    New_String = String[0] + "\t=\t" + New_String + "\n"
+
+    return New_String
+
+
+def Chng_fbrx_meas_spec_only_Slm(Selected_spc, band, FBRX_Meas_var, text_area):
+    new_text_content = ""
+    Check = False
+
+    target_word = f"[SUB6_n{band}_Calibration_Spec]"
+    FBRX_Spec = float(FBRX_Meas_var.get())
+    End_word = f"TX_Main_TEMP_ADC"
+
+    with open(Selected_spc, "r", encoding="utf-8") as file:
+        data_lines = file.readlines()
+    file.close()
+
+    for index, line in enumerate(data_lines):
+        New_String = Old_String = line
+        if line.startswith(target_word):
+            new_text_content += line
+            Check = True
+        # Gainstate를 for로 할 경우 너무 많은 반복 -> if 문으로 처리
+        elif Check and ("_FBRXGAIN_Pow_Index_" in line):
+            New_String = fbrx_spec_Slm(line, FBRX_Spec, text_area)
+            Change_Str = line.replace(Old_String, New_String)
+            new_text_content += Change_Str
+        elif Check and ("_FBRXGAIN_Code_Index_") in line:
+            New_String = fbrx_spec_Slm(line, FBRX_Spec * 100, text_area)
+            Change_Str = line.replace(Old_String, New_String)
+            new_text_content += Change_Str
+        elif Check and ("_FBRXFREQ_HPM_Pow") in line:
+            New_String = fbrxfreq_spec_Slm(line, FBRX_Spec, text_area)
+            Change_Str = line.replace(Old_String, New_String)
+            new_text_content += Change_Str
+        elif Check and ("_FBRXFREQ_LPM_Pow") in line:
+            New_String = fbrxfreq_spec_Slm(line, FBRX_Spec, text_area)
+            Change_Str = line.replace(Old_String, New_String)
+            new_text_content += Change_Str
+        elif Check and ("_FBRXFREQ_HPM_Code") in line:
+            New_String = fbrxfreq_spec_Slm(line, FBRX_Spec * 100, text_area)
+            Change_Str = line.replace(Old_String, New_String)
+            new_text_content += Change_Str
+        elif Check and ("_FBRXFREQ_LPM_Code") in line:
+            New_String = fbrxfreq_spec_Slm(line, FBRX_Spec * 100, text_area)
+            Change_Str = line.replace(Old_String, New_String)
+            new_text_content += Change_Str
+        elif Check and line.startswith(End_word):
+            new_text_content += line
+            Check = False
+
+        else:
+            new_text_content += line
+
+    with open(Selected_spc, "w", encoding="utf-8") as f:
+        f.writelines(new_text_content)
+    f.close()
+
+
+def fbrx_spec_Slm(line, FBRX_Spec, text_area):
+    String = [v for v in re.split("[=,\t\n]", line) if v]
+
+    text_area.insert(
+        tk.END,
+        f"{String[0]:<30} | {String[2]:>5}\t{String[3]:>5}\t  \u2192  ",
+    )
+    FBRX_Value = [float(String[2]), float(String[3])]
+    FBRX_mean = np.mean(FBRX_Value)
+    String[1] = round(FBRX_mean)
+    String[2] = round(String[1] - FBRX_Spec)
+    String[3] = round(String[1] + FBRX_Spec)
+
+    text_area.insert(tk.END, f"\t{String[2]:>5}\t{String[3]:>5}\n")
+    text_area.see(tk.END)
+    New_String = "\t".join(map(str, String[1:]))
+    New_String = String[0] + "\t=\t" + New_String + "\n"
+
+    return New_String
+
+
+def fbrxfreq_spec_Slm(line, FBRX_Spec, text_area):
+    String = [v for v in re.split("[=,\t\n]", line) if v]
+
+    text_area.insert(
+        tk.END,
+        f"{String[0]:<30} | {String[2]:>5}\t{String[3]:>5}\t  \u2192  ",
+    )
+    FBRX_Value = [float(String[2]), float(String[3])]
+    FBRX_mean = np.mean(FBRX_Value)
+    String[1] = round(FBRX_mean)
+    String[2] = round(String[1] - FBRX_Spec)
+    String[3] = round(String[1] + FBRX_Spec)
+
+    text_area.insert(tk.END, f"\t{String[2]:>5}\t{String[3]:>5}\n")
+    text_area.see(tk.END)
+    New_String = "\t".join(map(str, String[1:]))
+    New_String = String[0] + "\t=\t" + New_String + "\n"
+
+    return New_String
+
+
+def Chng_2G_rx_gain_spec_only_Slm(Selected_spc, rat, band, RX_Gain_2G_Spec_var, text_area):
+    RX_Gain_2G_Spec = float(RX_Gain_2G_Spec_var.get())
+    target_word = f"[{band}_Calibration_Spec]"
+    new_text_content = ""
+    Check = False
+    PRX_Gain_2G = []  # Spec only에서는 빈 리스트만 전달에서 오류 방지
+    with open(Selected_spc, "r", encoding="utf-8") as file:
+        data_lines = file.readlines()
+    file.close()
+
+    for index, line in enumerate(data_lines):
+        New_String = Old_String = line
+        if line.startswith(target_word):
+            new_text_content += line
+            Check = True
+            text_area.insert(tk.END, f"\n\n{target_word}\n\n")
+        # Gainstate를 for로 할 경우 너무 많은 반복 -> if 문으로 처리
+        elif Check & line.startswith("Rx_AGCOffset_0"):
+            String = Old_String = line
+            String = re.split("\t|\n", line)
+            String = [v for v in String if v]
+            New_String = root_2grx.rx_gain(PRX_Gain_2G, RX_Gain_2G_Spec, band, "Spec_Only", String, "None", text_area)
+            Change_Str = line.replace(Old_String, New_String)
+            new_text_content += Change_Str
+        elif Check & line.startswith("Rx_AGCOffset_1"):
+            String = Old_String = line
+            String = re.split("\t|\n", line)
+            String = [v for v in String if v]
+            New_String = root_2grx.rx_gain(PRX_Gain_2G, RX_Gain_2G_Spec, band, "Spec_Only", String, "None", text_area)
+            Change_Str = line.replace(Old_String, New_String)
+            new_text_content += Change_Str
+        elif Check & line.startswith("Rx_AGCOffset_2"):
+            String = Old_String = line
+            String = re.split("\t|\n", line)
+            String = [v for v in String if v]
+            New_String = root_2grx.rx_gain(PRX_Gain_2G, RX_Gain_2G_Spec, band, "Spec_Only", String, "None", text_area)
+            Change_Str = line.replace(Old_String, New_String)
+            new_text_content += Change_Str
+        elif Check & line.startswith("Rx_AGCOffset_3"):
+            String = Old_String = line
+            String = re.split("\t|\n", line)
+            String = [v for v in String if v]
+            New_String = root_2grx.rx_gain(PRX_Gain_2G, RX_Gain_2G_Spec, band, "Spec_Only", String, "None", text_area)
+            Change_Str = line.replace(Old_String, New_String)
+            new_text_content += Change_Str
+        elif Check and line.startswith("GMSK_Ref_Power"):
+            new_text_content += line
+            Check = False
+        else:
+            new_text_content += line
+
+    with open(Selected_spc, "w", encoding="utf-8") as f:
+        f.writelines(new_text_content)
+    f.close()
+
+
+def Chng_2g_tx_spec_only_Slm(
+    Selected_spc,
+    band,
+    GMSK_Spec_var,
+    GTxL_Spec_var,
+    GCode_Spec_var,
+    EPSK_Spec_var,
+    ETxL_Spec_var,
+    ECode_Spec_var,
+    text_area,
+):
+    # 2G TX FBRX Spec
+    GMSK_2G_Spec = float(GMSK_Spec_var.get())
+    GTxL_2G_Spec = float(GTxL_Spec_var.get())
+    GCode_2G_Spec = float(GCode_Spec_var.get())
+    EPSK_2G_Spec = float(EPSK_Spec_var.get())
+    ETxL_2G_Spec = float(ETxL_Spec_var.get())
+    ECode_2G_Spec = float(ECode_Spec_var.get())
+    target_word = f"[{band}_Calibration_Spec]"
+    new_text_content = ""
+    Check = False
+
+    with open(Selected_spc, "r", encoding="utf-8") as file:
+        data_lines = file.readlines()
+    file.close()
+
+    for index, line in enumerate(data_lines):
+        if line.startswith(target_word):
+            new_text_content += line
+            Check = True
+        # Gainstate를 for로 할 경우 너무 많은 반복 -> if 문으로 처리
+        elif Check & line.startswith("GMSK_Ref_Power"):
+            New_String = tx_power(GMSK_2G_Spec, band, "Ref", line, text_area)
+            Change_Str = line.replace(line, New_String)
+            new_text_content += Change_Str
+        elif Check & line.startswith("GMSK_TxL"):
+            New_String = tx_power(GCode_2G_Spec, band, "TxL", line, text_area)
+            Change_Str = line.replace(line, New_String)
+            new_text_content += Change_Str
+        elif Check & line.startswith("GMSK_Power_TxL"):
+            New_String = tx_power(GTxL_2G_Spec, band, "Power_TxL", line, text_area)
+            Change_Str = line.replace(line, New_String)
+            new_text_content += Change_Str
+        elif Check & line.startswith("EPSK_Ref_Power"):
+            New_String = tx_power(EPSK_2G_Spec, band, "Ref", line, text_area)
+            Change_Str = line.replace(line, New_String)
+            new_text_content += Change_Str
+        elif Check & line.startswith("EPSK_TxL"):
+            New_String = tx_power(ECode_2G_Spec, band, "TxL", line, text_area)
+            Change_Str = line.replace(line, New_String)
+            new_text_content += Change_Str
+        elif Check & line.startswith("EPSK_Power_TxL"):
+            New_String = tx_power(ETxL_2G_Spec, band, "Power_TxL", line, text_area)
+            Change_Str = line.replace(line, New_String)
+            new_text_content += Change_Str
+        elif Check & line.startswith("Channel_Comp_GMSK"):
+            new_text_content += line
+            Check = False
+        else:
+            new_text_content += line
+
+    with open(Selected_spc, "w", encoding="utf-8") as f:
+        f.writelines(new_text_content)
+    f.close()
+
+
+def tx_power_Slm(Spec_var, band, option, line, text_area):
+    String = line.strip().split("\t")
+    if option == "Power_TxL":
+        text_area.insert(tk.END, f"{String[0]:<30}| {String[2]:>5}\t{String[3]:>5}\t\t\u2192\t")
+        TxLevel = int(re.split("TxL|=|\t|\n", line)[1])
+        if band == "G085" or band == "G09":
+            TxP = (19 - TxLevel) * 2 + 5
+        else:
+            TxP = (15 - TxLevel) * 2
+        String[2] = round(int(TxP) - Spec_var)
+        String[3] = round(int(TxP) + Spec_var)
+        text_area.insert(tk.END, f"{String[2]:>5}\t{String[3]:>5}\n")
+    else:
+        text_area.insert(tk.END, f"{String[0]:<30}| {String[3]:>5}\t{String[4]:>5}\t\t\u2192\t")
+        text_area.see(tk.END)
+        Tx_value = [float(String[3]), float(String[4])]
+        Tx_mean = np.mean(Tx_value)
+        String[2] = round(int(Tx_mean))
+        String[3] = round(int(Tx_mean) - Spec_var)
+        String[4] = round(int(Tx_mean) + Spec_var)
+        text_area.insert(tk.END, f"{String[3]:>5}\t{String[4]:>5}\n")
+    text_area.see(tk.END)
+    New_String = "\t".join(map(str, String)) + "\n"
 
     return New_String
